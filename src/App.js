0be0b07f0b1c21254b7351da9926a86cb0a1dfe1
Alexander Khomenko <72103285+xomuss@users.noreply.github.com>
2021-06-09 16:04:35 +0300

    add increment method

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react'
 import Feedback from './Feedback'
 
-class App extends Component {
-  state = {
+const App = () => (
+
+  <Feedback initialState={{
     good: 0,
     neutral: 0,
     bad: 0
-  }
+  }} />
+)
 
-  hendleIncrement = () => {
-    this.setState((prevState) => ({
-      good: prevState.good + 1
-    }))
-  }
 
-  render() {
-    return (
-      <>
-        <h1>Please leave feedback</h1>
-        <Feedback initialFeedbac={this.state} />
-      </>
-    )
-  }
-}
+
 
 
 
--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Feedback = ({ initialFeedbac }) => (
-    <>
-        <div>
-            <button type="button" onClick={this.hendleIncrement}>Good</button>
-            <button type="button">Neutral</button>
-            <button type="button">Bad</button>
-        </div>
-        <h2>Statistics</h2>
-        <ul>
-            <li>Good: {initialFeedbac.good}</li>
-            <li>Neutral: {initialFeedbac.neutral}</li>
-            <li>Bad: {initialFeedbac.bad}</li>
-        </ul>
-    </>
-)
+
+class Feedback extends Component {
+
+
+    state = this.props.initialState
+
+    goodIncrement = () => {
+        this.setState((prevState) => ({
+            good: prevState.good + 1
+        }))
+    }
+    neutralIncrement = () => {
+        this.setState((prevState) => ({
+            neutral: prevState.neutral + 1
+        }))
+    }
+    badIncrement = () => {
+        this.setState((prevState) => ({
+            bad: prevState.bad + 1
+        }))
+    }
+
+    countTotalFeedback = () => {
+
+    }
+
+    render() {
+        const { good, neutral, bad } = this.state
+        return (
+            <>
+                <h1>Please leave feadback</h1>
+                <div>
+                    <button type="button" onClick={this.goodIncrement}>Good</button>
+                    <button type="button" onClick={this.neutralIncrement}>Neutral</button>
+                    <button type="button" onClick={this.badIncrement}>Bad</button>
+                </div>
+                <h2>Statistics</h2>
+                <ul>
+                    <li>Good: {good}</li>
+                    <li>Neutral: {neutral}</li>
+                    <li>Bad: {bad}</li>
+                </ul>
+            </>
+        )
+    }
+
+}
 
 export default Feedback;
